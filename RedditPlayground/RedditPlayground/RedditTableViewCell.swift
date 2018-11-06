447580f797b7da328b24482ed3e545817bf55fd2
Joe Szymanski <jszymanski@myyearbook.com>
2018-11-05 21:23:28 -0500

    Add basic content into the cells

--- RedditPlayground/RedditPlayground/RedditTableViewCell.swift
+++ RedditPlayground/RedditPlayground/RedditTableViewCell.swift
@@ -9,4 +9,8 @@
 import UIKit
 
 class RedditTableViewCell: UITableViewCell {
+    @IBOutlet weak var thumbnail: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var author: UILabel!
+    @IBOutlet weak var numComments: UILabel!
 }
--- RedditPlayground/RedditPlayground/ViewController.swift
+++ RedditPlayground/RedditPlayground/ViewController.swift
@@ -36,12 +36,59 @@ extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "RedditCell", for: indexPath)
+        guard let redditCell = cell as? RedditTableViewCell else { return cell }
         if indexPath.row < redditAPI.posts.count {
             let post = redditAPI.posts[indexPath.row]
-            cell.textLabel?.text = post.title
+            redditCell.title.text = post.title
+            redditCell.author.text = "Posted by \(post.author), \(post.createdAt.timeAgoString())"
+            redditCell.numComments.text = "\(post.numComments) comments"
         } else {
-            cell.textLabel?.text = "\(indexPath)"
+            redditCell.title.text = "\(indexPath)"
         }
-        return cell
+        return redditCell
+    }
+}
+
+extension Date {
+    func timeAgoString() -> String {
+        let components = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: self, to: Date())
+        if let years = components.year {
+            if years > 1 {
+                return "\(years) years ago"
+            } else {
+                return "1 year ago"
+            }
+        } else if let months = components.month {
+            if months > 1 {
+                return "\(months) months ago"
+            } else {
+                return "1 month ago"
+            }
+        } else if let days = components.day {
+            if days > 1 {
+                return "\(days) days ago"
+            } else {
+                return "1 day ago"
+            }
+        } else if let hours = components.hour {
+            if hours > 1 {
+                return "\(hours) hours ago"
+            } else {
+                return "1 hour ago"
+            }
+        } else if let minutes = components.minute {
+            if minutes > 1 {
+                return "\(minutes) minutes ago"
+            } else {
+                return "1 minute ago"
+            }
+        } else if let seconds = components.second {
+            if seconds > 1 {
+                return "\(seconds) seconds ago"
+            } else {
+                return "just now ago"
+            }
+        }
+        return ""
     }
 }
