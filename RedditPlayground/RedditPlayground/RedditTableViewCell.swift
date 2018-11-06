ab08c32060c60a000002481a11f2729328bca458
Joe Szymanski <jszymanski@myyearbook.com>
2018-11-05 20:17:52 -0500

    Add basic UITableView skeleton

---
+++ RedditPlayground/RedditPlayground/RedditTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  RedditTableViewCell.swift
+//  RedditPlayground
+//
+//  Created by Joe Szymanski on 11/5/18.
+//  Copyright © 2018 RedditPlayground. All rights reserved.
+//
+
+import UIKit
+
+class RedditTableViewCell: UITableViewCell {
+}
--- RedditPlayground/RedditPlayground/ViewController.swift
+++ RedditPlayground/RedditPlayground/ViewController.swift
@@ -12,9 +12,30 @@ class ViewController: UIViewController {
     /// Reddit API management class instance
     let redditAPI = RedditAPI()
 
+    /// Access to the table view
+    @IBOutlet weak var tableView: UITableView!
+    
+    /// Reddit posts to display in the table
+    var posts: [Int] = [0,0,0,0,0]
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         redditAPI.getTopPosts()
+        tableView.reloadData()
     }
 }
 
+extension ViewController: UITableViewDelegate {
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RedditCell", for: indexPath)
+        cell.textLabel?.text = "\(indexPath)"
+        return cell
+    }
+}
